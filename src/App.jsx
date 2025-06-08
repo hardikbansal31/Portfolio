3e916ea1cdba04f22bdb278ce76820577c1ee82f
Hardik <hardik@Neerajs-MacBook-Air.local>
2025-06-08 18:48:12 +0530

    intial setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Spinner from "react-bootstrap/Spinner";
 import Content from "./Content";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading (e.g., fetch or delay)
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2 seconds loading time
-
-    return () => clearTimeout(timer); // cleanup
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-white dark:bg-gray-900">
-        <Spinner animation="border" role="status" variant="primary">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </div>
-    );
-  }
-
   return <Content />;
 };
 
